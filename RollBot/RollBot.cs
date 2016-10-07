554c025e5793525f14557a175a6f7464d35911e7
unknown <khetke@gmail.com>
2016-10-06 18:09:23 -0600

    Added the bot class and some structuring

---
+++ RollBot/RollBot.cs
@@ -0,0 +1,73 @@
+﻿/***************
+ * Kamran Hetke
+ * 10/4/2016
+ * RollBot > RollBot.cs
+ ***************/
+
+/* Discord Specific */
+using Discord;
+using Discord.Commands;
+
+/* System */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RollBot
+{
+    class RollBot
+    {
+        DiscordClient discord;
+
+        public RollBot()
+        {
+            /* Settings */
+            char commandCharacter = '!'; // This is the character used to give RollBot Commands
+            /************/
+
+            /* Bot Configuration */
+            discord = new DiscordClient(x =>
+                {
+                    // Logs data to the console
+                    x.LogLevel = LogSeverity.Info;
+                    x.LogHandler = Log;
+                });
+
+            discord.UsingCommands(x =>
+                {
+                    x.PrefixChar = commandCharacter;
+                    x.AllowMentionPrefix = true;
+                });
+
+            var commands = discord.GetService<CommandService>();
+            /*********************/
+
+
+            /* Commands */
+            // !spooky gives a creative insult to "Spooky"
+            commands.CreateCommand("hello")
+                .Do(async (e) =>
+                    {
+                        await e.Channel.SendTTSMessage("Hello!");
+                    });
+
+            /************/
+
+
+            /* Connect To Discord */
+            discord.ExecuteAndWait(async () =>
+                {
+                    await discord.Connect("MjMzMDczODA3NTQyMTkwMDgx.CtYKfg.e0LNh0nHZl8z3ygnjkyMLmiafnc", TokenType.Bot); //"MjMzMDczODA3NTQyMTkwMDgx.CtYKfg.e0LNh0nHZl8z3ygnjkyMLmiafnc"
+                });
+            /**********************/
+
+        }
+
+        private void Log(object sender, LogMessageEventArgs e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
