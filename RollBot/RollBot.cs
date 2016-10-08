7f9be23a81e56258c9825046d612a6fda6ba73c7
Kamran Hetke <KHetke@gmail.com>
2016-10-07 22:00:33 -0600

    Added dice rolling functionality, positive modifiers

--- RollBot/RollBot.cs
+++ RollBot/RollBot.cs
@@ -1,6 +1,6 @@
 ﻿/***************
  * Kamran Hetke
- * 10/4/2016
+ * 10/4/2016 (Initial Creation)
  * RollBot > RollBot.cs
  ***************/
 
@@ -20,51 +20,72 @@ namespace RollBot
     class RollBot
     {
         DiscordClient discord;
+        CommandService commands;
 
         public RollBot()
         {
             /* Settings */
-            char commandCharacter = '!'; // This is the character used to give RollBot Commands
+            char commandCharacter = '!'; // This character is used to give RollBot Commands
             /************/
 
             /* Bot Configuration */
             discord = new DiscordClient(x =>
                 {
-                    // Logs data to the console
+                    // Logs various activities to the console
                     x.LogLevel = LogSeverity.Info;
                     x.LogHandler = Log;
                 });
 
-            discord.UsingCommands(x =>
+            discord.UsingCommands(x => 
                 {
+                    // Sets the method for using commands
                     x.PrefixChar = commandCharacter;
                     x.AllowMentionPrefix = true;
                 });
 
-            var commands = discord.GetService<CommandService>();
+            commands = discord.GetService<CommandService>(); // Initialize the discord.net command service
             /*********************/
 
-
-            /* Commands */
-            // !spooky gives a creative insult to "Spooky"
-            commands.CreateCommand("hello")
-                .Do(async (e) =>
-                    {
-                        await e.Channel.SendTTSMessage("Hello!");
-                    });
-
-            /************/
-
+            /* Command Registry */
+            // !hello says hello!
+            RegisterHelloCommand();
+            // !roll rolls dice within a range of 1-1000
+            RegisterRollCommand();
+            /********************/
 
             /* Connect To Discord */
             discord.ExecuteAndWait(async () =>
                 {
-                    await discord.Connect("MjMzMDczODA3NTQyMTkwMDgx.CtYKfg.e0LNh0nHZl8z3ygnjkyMLmiafnc", TokenType.Bot); //"MjMzMDczODA3NTQyMTkwMDgx.CtYKfg.e0LNh0nHZl8z3ygnjkyMLmiafnc"
+                    await discord.Connect("", TokenType.Bot); // Put your token here
                 });
             /**********************/
-
         }
 
+        /* Commands */
+        private void RegisterHelloCommand()
+        {
+            commands.CreateCommand("hello")
+                .Do(async (e) =>
+                {
+                    Console.WriteLine("!Hello"); // Logs the command used to the console
+                    await e.Channel.SendTTSMessage("Hello!");
+                });
+        }
+        private void RegisterRollCommand()
+        {
+            commands.CreateCommand("roll")
+                .Parameter("dieParameters") // Parameters following the !roll command are stored in "dieParameters"
+                .Do(async (e) =>
+                {
+                    string dieParameters = e.GetArg("dieParameters"); 
+                    Console.WriteLine("!roll " + dieParameters); // Logs the command used to the console
+                    Roll roll = new Roll(dieParameters);
+                    Console.WriteLine("Roll total is " + roll.total); // Logs the die roll to the console
+                    await e.Channel.SendMessage("Roll = " + roll.total);
+                });
+        }
+
+        /* Log to Console */
         private void Log(object sender, LogMessageEventArgs e)
         {
             Console.WriteLine(e.Message);
